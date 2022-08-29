505a3d8cd67c66fa0c19bd0328bc2f2887d994db
lucia gonzalez <luciabegeve@gmail.com>
2022-08-29 17:51:57 -0300

    funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+//agregar manejador de eventos
+let btnVerMas = document.getElementById('btnVerMas');
+console.log(btnVerMas);
+btnVerMas.addEventListener('click', verMas)
+
+// si agrego los parametros en la funcion, el evento se realiza antes del click
+//cuando una funcionListener('click', ()=>{verMas(parametro)}) tiene parametros debo agregar funcion anonima 
+//btnVerMas.addEvent
 function cambiarTitulo(){
     console.log('desde la funcion cambiar titulo');
 //traer elemento del html
@@ -11,3 +19,26 @@ titulo.innerHTML = 'Nuevo titulo de prueba'
 titulo.className = 'display-4 tituloPrincipal'
 
 }
+
+function verMas(){
+    console.log('desde la funcion ver mas')
+//traer el elemento padre(section)
+let sectionPadre = document.getElementById('noticia');
+//crear el elemento nuevo html(nodo/objeto)
+let parrafoNuevo = document.createElement('p'); //<p></p>
+//preparar el elemento
+parrafoNuevo.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut a suscipit, enim, molestiae sunt inventore tenetur laboriosam explicabo optio, nobis dicta ab eveniet? Rem delectus voluptatum saepe inventore commodi deleniti quaerat, non quisquam? Modi ullam tenetur at, facere quo explicabo officia nihil quod, laudantium sapiente corrupti similique repellendus illum beatae fugiat eligendi! Repudiandae rem expedita at voluptate consequuntur accusamus laboriosam mollitia beatae porro excepturi ea id sit, magnam perspiciatis tempora libero provident a ab voluptatibus itaque. Non asperiores quod voluptate recusandae fuga eligendi, nostrum qui doloremque reiciendis, id omnis accusantium. Doloremque animi atque fugit voluptate saepe harum natus sint a velit, dolorem numquam corrupti eaque corporis assumenda. Suscipit ullam delectus velit dolore et ex eligendi magnam, quibusdam eos placeat pariatur illum ad eum atque, voluptatem nostrum saepe nemo ipsam odio nesciunt, harum fugit aut. Incidunt, minima pariatur tenetur consequatur cum facere quia soluta quisquam omnis adipisci earum harum aliquid, obcaecati iure corporis error ut rem. Obcaecati cupiditate et unde ab nihil doloremque necessitatibus est repellat pariatur quo! Magnam tempore sunt labore sapiente dolorum iusto eveniet exercitationem? Eum at ab facere quam vero voluptatem totam ipsum corrupti qui neque veniam obcaecati quo, perferendis consequatur recusandae facilis, aperiam sit, nihil reprehenderit sint.'
+parrafoNuevo.className = 'lead';
+//insertar el nuevo elemento en el DOM
+sectionPadre.appendChild(parrafoNuevo); //agrega el nodo hijo al final
+sectionPadre.prepend(parrafoNuevo); //agrega el nodo hijo al principio
+sectionPadre.insertBefore(parrafoNuevo, btnVerMas);
+//cambiar el boton
+btnVerMas.innerHTML = 'Ocultar';
+btnVerMas.className = 'btn btn-outline-danger' 
+//nota para acceder al texto de un input
+document.getElementById('buscador').value
+
+}
+
+
